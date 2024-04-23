ea62f274117153cdc693d714dbdd55eff5105489
Shikha <110949508+SH1kha1@users.noreply.github.com>
2024-04-23 06:04:13 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
 
+  var addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+  addToCartButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      console.log('تمت إضافة المنتج إلى السلة!');
+    });
+  });
+});
