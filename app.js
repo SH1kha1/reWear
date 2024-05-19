14d9afc8fa7abace21f400636ea8f37f5c250d13
Deemah Almutairi <163601138+Deemah8@users.noreply.github.com>
2024-05-19 17:43:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
+const connectDB = require('./config/db');
+const session = require('express-session');
+const passport = require('passport');
+const MongoStore = require('connect-mongo');
 
 const app = express();
 const PORT = 3000;
 
+// Connect passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use('/', require('./routes/auth'));
+
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Connect to Database
+connectDB();
 
 // Route to handle the root path
 app.get('/', (req, res) => {
