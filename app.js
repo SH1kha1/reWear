ae403c93da28dc9a327a5c1151f8a8245938a60d
Deemah Almutairi <163601138+Deemah8@users.noreply.github.com>
2024-05-22 19:39:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,24 +4,35 @@ const express = require('express');
 const path = require('path');
 const connectDB = require('./config/db');
 const session = require('express-session');
-const passport = require('passport');
-const MongoStore = require('connect-mongo');
+const { collection } = require('mongoose');
 
 const app = express();
 const PORT = 3000;
 
-// Connect passport
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Routes
-app.use('/', require('./routes/auth'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to Database
 connectDB();
+
+app.get('/public/signInUp', (req, res) => {
+  res.render('signInUp');
+})
+
+app.post('/public/signInUp', async (req, res) => {
+
+  const data = {
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  }
+
+  const userData = await collection.insertMany(data);
+  console.log(userData);
+})
 
 // Route to handle the root path
 app.get('/', (req, res) => {
