26fc0c85b2345eab5984073ccbefbfa63a420e2a
Deemah Almutairi <163601138+Deemah8@users.noreply.github.com>
2024-05-22 19:40:41 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,10 +2,28 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const connectDB = async() => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);//change (process.env.MONGODB_URI) to (mongodb://localhost:27017/reWear)
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`Database Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
     }
 }
+
 module.exports = connectDB;
+
+const loginSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    emal: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const collection = new mongoose.model("users", loginSchema);
